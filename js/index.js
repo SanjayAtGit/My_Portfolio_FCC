60564e31bc5f58ecc1e1b8786cf3a766d15d3e57
Sanjay Tailor <Sanjay@Sanjays-MacBook-Pro.local>
2016-08-08 16:59:22 -0400

    started repo

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.nav li a').click(function(e) {
+
+        $('.nav li').removeClass('active');
+
+        var $parent = $(this).parent();
+        if (!$parent.hasClass('active')) {
+            $parent.addClass('active');
+        }
+        // e.preventDefault();
+    });
+  
+    $('.navbar-brand').click(function(e) {
+      $('.nav li').removeClass('active');
+    });
+});
